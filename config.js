ea18bb7221794f1c663d0001525940a31243c543
aod3826 <pantipa3826@gmail.com>
2026-02-08 17:00:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   liffId: "YOUR_LIFF_ID",
-  scriptUrl: "YOUR_SCRIPT_URL",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbzKGEIllWVMwLiI60gzOW_I7FpRnu7MGT8dsORN58q0LD0pPhOJSi5KiSZqesg-Z4i3/exec",
   googleMapsKey: "YOUR_GOOGLE_MAPS_KEY",
   lineNotifyToken: "YOUR_LINE_NOTIFY_TOKEN"
 };
